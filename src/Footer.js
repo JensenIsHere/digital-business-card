dd2d92a628ed00f24d9ed22bfb351c1b644219a1
Jensen Ruud <jensen.is.testing@gmail.com>
2022-06-05 20:35:35 -0500

    Created all component files, created containers for all elements

---
+++ src/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer className="footer--container">
+      <h6>Twitter</h6>
+      <h6>Facebook</h6>
+      <h6>Instagram</h6>
+      <h6>GitHub</h6>
+    </footer>
+  );
+}
---
+++ src/Interests.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Interests() {
+  return (
+    <div className="interests--container">
+      <h2 className="interests--title">Interests</h2>
+      <p className="interests--text">I like stuff.</p>
+    </div>
+  );
+}
